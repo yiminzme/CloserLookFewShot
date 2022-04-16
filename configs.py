bf4a4e7d29fbd4957b7f1b58c255dee3d2042f78
vinc <yiminzme@gmail.com>
2022-04-16 15:29:44 +0000

    update save_dir

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+save_dir                    = '/home/yimin/vinc/2022NLP/CloserLookFewShot/CloserLookFewShot'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
