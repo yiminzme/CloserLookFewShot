efce6701319a38b9257040955d8789ee685e1ca1
tc_cloud <yiminzme@gmail.com>
2022-04-18 02:10:11 +0800

    update config

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-save_dir                    = '/home/yimin/vinc/2022NLP/CloserLookFewShot/CloserLookFewShot'
+save_dir                    = '/home/ubuntu/vinc/CloserLookFewShot'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-now="`date +%Y%m%d%H%M%S`";
-now_format="`date +%Y/%m/%d %T`";
+now="`date +%Y%m%d_%H%M%S`";
+now_format="`date +"%Y/%m/%d %T"`";
 echo $now_format;
 # execute run_1.ipynb in background, and save output to nohup_1.out
 # nohup jupyter nbconvert --to notebook --execute run.ipynb --output run_1.ipynb > nohup_$now.out &
-nohup python -u ./train.py --dataset CUB --model Conv4 --method maml_approx --train_aug > nohup_$now.out &
+nohup python3 -u ./train.py --dataset CUB --model Conv4 --method maml_approx --train_aug > nohup_$now.out &
 
 # print pid
 echo kill $!
