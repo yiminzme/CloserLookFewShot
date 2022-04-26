c6b4f8ce804c2deb0b7cf202e5312909cadbef30
tc_cloud <yiminzme@gmail.com>
2022-04-26 15:34:27 +0800

    improve execution workflow

--- run.sh
+++ run.sh
@@ -1,9 +1,24 @@
-now="`date +%Y%m%d_%H%M%S`";
-now_format="`date +"%Y/%m/%d %T"`";
-echo $now_format;
-# execute run_1.ipynb in background, and save output to nohup_1.out
-# nohup jupyter nbconvert --to notebook --execute run.ipynb --output run_1.ipynb > nohup_$now.out &
-nohup python3 -u ./train.py --dataset CUB --model Conv4 --method maml_approx --train_aug > nohup_$now.out &
+# print start info
+echo 
+TZ="Asia/HongKong"
+curr_time="`date +"%Y%m%d_%H_%M_%S"`"
+curr_sec=$(date +%s)
+echo "Start $curr_time"
+echo kill $$
+echo
 
-# print pid
-echo kill $!
+# build and execute command
+cmd="python3 -u ./train.py --dataset CUB --model Conv4 --method maml_approx --train_aug"
+# cmd="jupyter nbconvert --to notebook --execute run.ipynb --output run_1.ipynb"
+log_file="\"log/$curr_time maml_approx.log\""
+
+final_cmd="{ $cmd; } &>> $log_file"
+echo $final_cmd
+eval $final_cmd
+
+# print end info
+echo
+curr_time="`date +"%Y%m%d_%H_%M_%S"`"
+echo "End $curr_time"
+elapsed_time=$(($(date +%s)-$curr_sec))
+echo "Elapsed time ${elapsed_time}s"
